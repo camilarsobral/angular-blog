7b0f42af5263cd08e0773324fd927c249b3d118c
camilarsobral <camilarsobral@hotmail.com>
2023-10-17 12:53:26 -0300

    data binding

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-big-card',
-  templateUrl: './big-card.component.html',
-  styleUrls: ['./big-card.component.css']
+	selector: 'app-big-card',
+	templateUrl: './big-card.component.html',
+	styleUrls: ['./big-card.component.css']
 })
 export class BigCardComponent implements OnInit {
 
-  constructor() { }
+	@Input()
+	photoCover:string = ''
+	@Input()
+	cardTitle:string = ''
+	@Input()
+	cardDescription:string = ''
+	@Input()
+	cardHref:string = ''
 
-  ngOnInit(): void {
-  }
+	constructor() { }
+
+	ngOnInit(): void {
+	}
 
 }
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-small-card',
-  templateUrl: './small-card.component.html',
-  styleUrls: ['./small-card.component.css']
+	selector: 'app-small-card',
+	templateUrl: './small-card.component.html',
+	styleUrls: ['./small-card.component.css']
 })
 export class SmallCardComponent implements OnInit {
+	@Input()
+	photoCover:string =''
+	@Input()
+	cardTitle:string = ''
+	@Input()
+	cardHref:string = ''
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
 }
